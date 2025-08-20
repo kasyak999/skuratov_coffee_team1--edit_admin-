18f48e43de78c59d7013ed73529ce656ec74664c
Максим <kasyak999@yandex.ru>
2025-08-20 18:14:07 +0300

    гз

--- src/app/core/celery_worker.py
+++ src/app/core/celery_worker.py
@@ -3,7 +3,7 @@ from celery import Celery
 from app.core.config import settings
 
 CELERY_HOST = 'localhost' if settings.DEBUG else 'redis_service'
-BROKER = f'redis://:{settings.redis_pass}@{CELERY_HOST}:6379/0'
+BROKER = f'redis://:{settings.redis_pass}@{CELERY_HOST}:6380/0'
 
 celery_app = Celery("Селери", broker=BROKER, backend=BROKER)
 celery_app.autodiscover_tasks([
